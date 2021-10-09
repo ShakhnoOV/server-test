fc69ffc381d88f847c3ec4c4d82d08d2f796419a
ShakhnoOV <50460093+ShakhnoOV@users.noreply.github.com>
2021-10-09 15:29:10 +0300

    f7

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send('POHOL na HUI')
+    res.send('POHOL na HUI i sosi bibu')
 })
 
 app.post('/', (req, res) => {
