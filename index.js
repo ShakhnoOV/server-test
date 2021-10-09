4f72408073378213901e8d97374e3cf899360b0e
ShakhnoOV <50460093+ShakhnoOV@users.noreply.github.com>
2021-10-09 15:07:23 +0300

    [feature/server]ISSUE-4 | cookie

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('POHOL na HUI')
 })
 
 app.post('/', (req, res) => {
-    res.send(`${req.body.message} 123 ${req.body.idu}`);
+    res.send(`${req.cookies.user} ${req.body.message} ${req.body.idu}`);
+})
+
+app.get('/setUserName', (req, res) => {
+    res.cookie('user', req.query.userName, { httpOnly: true });
+    res.send(`${req.query.userName} cookie was set`);
 })
 
 app.listen(port, () => {
